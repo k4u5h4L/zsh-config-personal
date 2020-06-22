ea3d06855eb6ee05ca56b5cc42758fceed3ccad3
Kaushal <kaushal.v.bhat@gmail.com>
2020-06-22 13:11:44 +0530

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -2,7 +2,6 @@ alias update='sudo pacman -Syu'
 alias f="fuck"
 alias c="clear"
 alias s="sudo"
-alias rmjunk="sudo apt autoremove"
 alias shn="shutdown now"
 alias l='ls'
 alias list='ls'
@@ -19,7 +18,7 @@ mkcd ()
 
 alias vpn-connect='sudo protonvpn c -r'
 alias vpn-disconnect='sudo protonvpn disconnect'
-alias p='python'
+alias py='python'
 alias n='node'
 alias pipes='sh /home/sl4ught3r/pipes.sh'
 alias nmn='nodemon'
