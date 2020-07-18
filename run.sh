c2161b78b902b5d73d6825b30377769a8583ff51
Kaushal <kaushal.v.bhat@gmail.com>
2020-07-18 14:37:32 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 
-
-
 # This script compiles and executes a c or c++ file saving yourself from typing two commands
-
 # this also checks the file extension so that you don't have too
-
-
-
 # to make it even easier, alias the execution of the file. Run these commands below
 
-
-
 # chmod +x run.sh
-
 # alias run='./run.sh'
-
-
 
 # if you are going to be using this command often, consider adding the alias to your .bashrc or .zshrc
 
@@ -34,14 +23,15 @@ then
 
             echo "Compiled successfully! Running..."
 
-        ./a.out
+            ./a.out
+
+
 
         else
 
             echo "Compilation stopped"
 
         fi
-
 
     elif [ ${1: -4} == ".cpp" ]
 
@@ -49,19 +39,17 @@ then
 
         if g++ $1 ; then
 
-        echo "Compiling a C++ program..."
+            echo "Compiling a C++ program..."
 
-        echo "Compiled successfully! Running..."
+            echo "Compiled successfully! Running..."
 
-        ./a.out
+            ./a.out
 
         else
 
             echo "Compilation stopped"
 
         fi
-
-
 
     else
 
