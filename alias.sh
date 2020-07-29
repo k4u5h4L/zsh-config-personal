d92dcedbb07fddaa5d0659598ec7f85137988f3a
Kaushal <kaushal.v.bhat@gmail.com>
2020-07-29 13:41:16 +0530

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -2,6 +2,7 @@ alias update='sudo pacman -Syu'
 alias f="fuck"
 alias c="clear"
 alias s="sudo"
+alias rmjunk="sudo apt autoremove"
 alias shn="shutdown now"
 alias l='ls'
 alias list='ls'
@@ -16,10 +17,27 @@ mkcd ()
   cd -P -- "$1"
 }
 
+cdl() {
+        local dir="$1"
+        local dir="${dir:=$HOME}"
+        if [[ -d "$dir" ]]; then
+                cd "$dir" >/dev/null; ls --color=auto
+        else
+                echo "bash: cdls: $dir: Directory not found"
+        fi
+}
+
+CD() {
+        cd "$1"; ls;
+}
+# alias cd='CD'
+
+alias run='/usr/bin/run.sh'
 alias vpn-connect='sudo protonvpn c -r'
 alias vpn-disconnect='sudo protonvpn disconnect'
 alias py='python'
+alias py3='python3'
+alias py-v='python --version'
 alias n='node'
 alias pipes='sh /home/sl4ught3r/pipes.sh'
 alias nmn='nodemon'
-
