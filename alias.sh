b301473a337e8670886f0db666a84000ef2217bf
Kaushal <kaushal.v.bhat@gmail.com>
2020-09-02 18:16:04 +0530

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,15 +1,33 @@
 alias update='sudo pacman -Syu'
-alias f="fuck"
 alias c="clear"
 alias s="sudo"
-alias rmjunk="sudo apt autoremove"
+alias rmjunk="sudo pacman -Qdt"
 alias shn="shutdown now"
 alias l='ls'
 alias list='ls'
 alias add='sudo pacman -S'
-alias 69='./nice.sh'
 alias ping-test='ping -c 3 google.com'
 alias orphans='sudo pacman -Qdtq'
+
+list-pacman-cache() {
+        echo "Number of pacman cache packages:"
+        sudo ls /var/cache/pacman/pkg/ | wc -l
+
+        echo "Amount of storage used:"
+        du -sh /var/cache/pacman/pkg/
+}
+
+alias check-cache='list-pacman-cache'
+
+clean-pacman-cache() {
+        echo "Cleaning pacman cache except last one version"
+        sudo paccache -rk 1
+
+        echo "Cleaning pacman cache of uninstalled packages"
+        sudo pacman -Sc
+}
+
+alias clean-cache='clean-pacman-cache'
 
 mkcd ()
 {
@@ -32,7 +50,9 @@ CD() {
 }
 # alias cd='CD'
 
-alias run='/usr/bin/run.sh'
+alias run='~/run.sh'
+alias topdf='/usr/bin/topdf.sh'
+
 alias vpn-connect='sudo protonvpn c -r'
 alias vpn-disconnect='sudo protonvpn disconnect'
 alias py='python'
@@ -41,3 +61,12 @@ alias py-v='python --version'
 alias n='node'
 alias pipes='sh /home/sl4ught3r/pipes.sh'
 alias nmn='nodemon'
+alias weather='curl http://wttr.in/$1'
+alias conda-deact='conda deactivate'
+
+activate-c() {
+        conda activate $1 
+}
+
+alias conda-act='activate-c'
+
