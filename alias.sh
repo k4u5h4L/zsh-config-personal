9d031f77a80ba1d0d5e6f3592b26bfb0675f55ad
Kaushal <kaushal.v.bhat@gmail.com>
2021-05-25 21:58:22 +0530

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -19,37 +19,37 @@ alias start-mongo='sudo systemctl start mongodb'
 alias start-maria='sudo systemctl start mariadb'
 
 list-pacman-cache() {
-	echo "Number of pacman cache packages:"
-	sudo ls /var/cache/pacman/pkg/ | wc -l
+        echo "Number of pacman cache packages:"
+        sudo ls /var/cache/pacman/pkg/ | wc -l
 
-	echo "Amount of storage used:"
-	du -sh /var/cache/pacman/pkg/
+        echo "Amount of storage used:"
+        du -sh /var/cache/pacman/pkg/
 }
 
 alias check-cache='list-pacman-cache'
 
 clean-pacman-cache() {
-	echo "Cleaning pacman cache except last one version"
-	sudo paccache -rk 1
+        echo "Cleaning pacman cache except last one version"
+        sudo paccache -rk 1
 
-	echo "Cleaning pacman cache of uninstalled packages"
-	sudo pacman -Sc
+        echo "Cleaning pacman cache of uninstalled packages"
+        sudo pacman -Sc
 }
 
 alias clean-cache='clean-pacman-cache'
 
 git-commit () {
-	git add .
-	git commit -m $1
+        git add .
+        git commit -m $1
 }
 
 git-remote-change() {
-	git remote rm origin
-	git remote add origin $1
+        git remote rm origin
+        git remote add origin $1
 }
 
 check-devices() {
-	sudo nmap -sn 192.168.1.0/24
+        sudo nmap -sn 192.168.1.0/24
 }
 
 mkcd ()
@@ -69,7 +69,7 @@ cdl() {
 }
 
 CD() {
-	cd "$1"; ls;
+        cd "$1"; ls;
 }
 # alias cd='CD'
 
@@ -91,23 +91,46 @@ alias conda-deact='conda deactivate'
 alias stop-maria='sudo systemctl stop mariadb.service'
 
 activate-c() {
-	conda activate $1 
+        conda activate $1 
+}
+
+compress-video() {
+        ffmpeg -i $1 -c:a aac -b:a 128k -c:v libx265 -crf 23 $2
 }
 
 conda-create-env() {
-	conda create --name $1
+        conda create --name $1
 }
 
 alias conda-act='activate-c'
 
 create-venv() {
-	python3 -m venv $1
-	# mv $1 ~/venvs/$1/
+        python3 -m venv $1
+        # mv $1 ~/venvs/$1/
 }
 start-venv() {
-	source $1/bin/activate
+        source $1/bin/activate
 }
 
 alias list-venvs='ls ~/venvs/'
+generate-pwa-assets() {
+        npx pwa-asset-generator $1 $2
+}
+
+alias vsc-install='code --install-extension'
+
+# alias extract-rar='unrar x $1'
+extract-rar() {
+        unrar x $1
+}
 
 alias stop-venv='deactivate'
+
+alias download-dir='wget --recursive --no-parent'
+alias download-dir-no-index='wget -r -np -R "index.html*"'
+
+alias open-postgres='sudo -iu postgres'
+alias start-postgres='sudo systemctl start postgresql.service'
+alias stop-postgres='sudo systemctl stop postgresql.service'
+
+alias check-package-installed='sudo pacman -Qs'
